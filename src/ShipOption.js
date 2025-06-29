7dc7ebf58cf6fc6f4507d58bc85e05dc76f54544
Cooper Chen <c1336873249@gmail.com>
2025-06-29 17:03:15 -0400

    fixed css for smaller screen on placing ships

--- src/ShipOption.js
+++ src/ShipOption.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ShipOptions({ isFlipped, activeShip, placedShips, handleShipOptionClick }) {
   return (
-    <div className={'ship-options'}>
+    <div className={`ship-options ${isFlipped ? 'flipped' : ''}`}>
       {!placedShips.includes('carrier') && (<div className={`ship-option carrier ${activeShip === 'carrier' ? 'selected' :''} ${isFlipped ? 'flipped' : ''}`} data-length={5} onClick={() => handleShipOptionClick('carrier')}></div>)}
       {!placedShips.includes('battleship') && (<div className={`ship-option battleship ${activeShip === 'battleship' ? 'selected' :''} ${isFlipped ? 'flipped' : ''}`} data-length={4} onClick={() => handleShipOptionClick('battleship')}></div>)}
       {!placedShips.includes('cruiser') && (<div className={`ship-option cruiser ${activeShip === 'cruiser' ? 'selected' :''} ${isFlipped ? 'flipped' : ''}`} data-length={3} onClick={() => handleShipOptionClick('cruiser')}></div>)}
